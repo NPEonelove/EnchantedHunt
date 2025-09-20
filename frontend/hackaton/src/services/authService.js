9acbfc8a9d3559f9b43ce98a7b4e78dd3df1cec6
HATEBOY1 <hateboy2005@gmail.com>
2025-09-20 14:56:00 +0400

    Добавил демо API

---
+++ frontend/hackaton/src/services/authService.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:8000';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const authService = {
+  login: async (email, password) => {
+    const response = await api.post('/auth/login', { email, password });
+    return response.data;
+  },
+
+  register: async (userData) => {
+    const response = await api.post('/auth/register', userData);
+    return response.data;
+  },
+
+  getProfile: async () => {
+    const response = await api.get('/auth/profile');
+    return response.data;
+  },
+
+  logout: async () => {
+    // Очистка на клиенте
+    localStorage.removeItem('token');
+  }
+};
+
+export default api;
